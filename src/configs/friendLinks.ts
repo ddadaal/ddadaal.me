4c161d30192701b2699bf6372a89e9fc966eb6b3
Chen Junda <smallda@outlook.com>
2019-04-08 22:14:10 +0800

    change friend link

--- src/configs/friendLinks.ts
+++ src/configs/friendLinks.ts
@@ -1,6 +1,6 @@
 const friendLinks = [
   { name: "idealclover", description: "翠翠酱的个人网站", link: "https://idealclover.top" },
-  { name: "FocusHour", description: "Sephidator的iOS应用", link: "https://github.com/Sephidator/FocusHour" },
+  { name: "Sephidator", description: "Sephidator的个人博客", link: "https://sephidator.xyz" },
   { name: "iznauy", description: "个人博客", link: "https://iznauy.github.io/" },
   { name: "Aironoria", description: "陈俊宇的个人博客", link: "https://aironoria.github.io" },
 ];
