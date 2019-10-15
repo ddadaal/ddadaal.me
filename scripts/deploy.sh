dd020abf9f817ee5dcc62d4ec53ad08f33ba3c79
Chen Junda <smallda@outlook.com>
2019-10-15 10:47:20 +0800

    fix build error

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CO_REF="git.coding.net/viccrubs/viccrubs.coding.me.git"
-GITHUB_REF="github.com/vicblog/vicblog/vicblog.github.io"
+GITHUB_REF="github.com/vicblog/vicblog.github.io"
 FQDN="daacheen.me"
 
 cd ./public
@@ -13,7 +13,7 @@ git commit -m "Update Blog By TravisCI With Build $TRAVIS_BUILD_NUMBER"
 
 # Coding Pages
 echo "[deploy bot] Deploying to Coding.NET..."
-git push --force --quiet "https://daacheen:${CODINGNET_TOKEN}@${CO_REF}" master:master
+git push --force --quiet "https://viccrubs:${CODINGNET_TOKEN}@${CO_REF}" master:master
 
 # GitHub Pages
 echo "[deploy bot] Deploying to GitHub Pages..."
