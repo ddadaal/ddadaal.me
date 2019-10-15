d076ff62bde11f71c2758ea39a9355767da08e6c
Chen Junda <smallda@outlook.com>
2019-10-15 10:41:33 +0800

    update ci script

---
+++ scripts/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CO_REF="git.coding.net/viccrubs/viccrubs.coding.me.git"
+GITHUB_REF="github.com/vicblog/vicblog/vicblog.github.io"
+FQDN="daacheen.me"
+
+cd ./public
+echo "pages.$FQDN" > CNAME
+git init
+git config user.name "daacheen-deploy-bot"
+git config user.email "daacheen@outlook.com"
+git add .
+git commit -m "Update Blog By TravisCI With Build $TRAVIS_BUILD_NUMBER"
+
+# Coding Pages
+echo "[deploy bot] Deploying to Coding.NET..."
+git push --force --quiet "https://daacheen:${CODINGNET_TOKEN}@${CO_REF}" master:master
+
+# GitHub Pages
+echo "[deploy bot] Deploying to GitHub Pages..."
+git push --force --quiet "https://daacheen:${GITHUB_TOKEN}@${GITHUB_REF}" master:master
