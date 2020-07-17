05a038ba0f241a78ece18f8c3a68f29a07b1fa58
Chen Junda <ddadaal@outlook.com>
2020-07-17 08:46:10 +0800

    disable coding.net deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,15 +10,16 @@ git clone --depth=1 "https://$GITHUB_REF" existing
 cp -r public/* existing/
 
 cd ./existing
-echo "pages.$FQDN" > CNAME
+# echo "pages.$FQDN" > CNAME
+echo "$FQDN" > CNAME
 git config user.name "ddadaal-deploy-bot"
 git config user.email "ddadaal@outlook.com"
 git add .
 git commit -m "Update By GitHub Actions to commit $(echo $GITHUB_SHA | head -c 6)"
 
 # Coding Pages
-echo "[deploy bot] Deploying to Coding.NET..."
-git push --quiet "https://${CODINGNET_USERNAME}:${CODINGNET_TOKEN}@${CO_REF}" master:master
+# echo "[deploy bot] Deploying to Coding.NET..."
+# git push --quiet "https://${CODINGNET_USERNAME}:${CODINGNET_TOKEN}@${CO_REF}" master:master
 
 # GitHub Pages
 echo "[deploy bot] Deploying to GitHub Pages..."
