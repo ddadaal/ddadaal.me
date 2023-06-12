28ffb369739570617793dfeb565d21922ea8f322
Chen Junda <ddadaal@outlook.com>
2023-06-12 11:45:31 +0800

    fix: heading title center

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -4,7 +4,7 @@ export const Heading = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div className="bg-neutral px-4 py-8">
       <div className="max-w-7xl mx-auto min-h-[256px] flex justify-center text-center text-neutral-content">
-        <div className="flex flex-col animate-slide-up">
+        <div className="flex flex-col justify-center animate-slide-up">
           {children}
         </div>
       </div>
