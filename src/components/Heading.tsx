ba98bd2fe1cb78fb8670b18d765ab8087fb3225c
Chen Junda <ddadaal@outlook.com>
2023-06-12 10:32:04 +0800

    fix: article title center

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -3,8 +3,8 @@ import { PropsWithChildren } from "react";
 export const Heading = ({ children }: PropsWithChildren<{}>) => {
   return (
     <div className="bg-neutral px-4 py-8">
-      <div className="max-w-7xl mx-auto min-h-[256px] flex justify-center items-center text-neutral-content">
-        <div className="flex flex-col items-center animate-slide-up">
+      <div className="max-w-7xl mx-auto min-h-[256px] flex justify-center text-center text-neutral-content">
+        <div className="flex flex-col animate-slide-up">
           {children}
         </div>
       </div>
