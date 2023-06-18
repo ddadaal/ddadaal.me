71f69905c43b26126fb2fa7f889f66764cb977b3
Chen Junda <ddadaal@outlook.com>
2023-06-18 17:10:41 +0800

    fix: toc max depth to 3

--- src/components/article/ArticleToc.tsx
+++ src/components/article/ArticleToc.tsx
@@ -1,23 +1,44 @@
 "use client";
 
-import { Toc } from "@stefanprobst/rehype-extract-toc";
+import { Toc, TocEntry } from "@stefanprobst/rehype-extract-toc";
 import { useEffect } from "react";
 import { MdToc } from "react-icons/md";
 import { Localized } from "src/i18n";
 
-interface Props {
-  toc: Toc;
-}
 
 function isWindowBetween(element: HTMLElement | null): boolean {
   return !!element && element.getBoundingClientRect().top >= 2;
+}
+
+const MAX_DEPTH = 3;
+
+const TocMenuEntry = ({ entry: { depth, value, children, id } }: { entry: TocEntry }) => {
+
+  return (
+    <li>
+      <a data-tocid={id} href={"#" + id}>
+        {value}
+      </a>
+      {(depth <= MAX_DEPTH && children && children.length > 0) ? (
+        <ul>
+          {children.map((x) => <TocMenuEntry key={x.id} entry={x} />)}
+        </ul>
+      ) : undefined}
+    </li>
+  );
+};
+
+interface Props {
+  toc: Toc
 }
 
 export const ArticleToc = ({ toc }: Props) => {
 
   useEffect(() => {
 
-    const tocItemElements: HTMLElement[] = [];
+    const tocItemElements = Array.from(document.querySelectorAll("[data-tocid]"));
+
+    if (tocItemElements.length === 0) { return; }
 
     let currentIndex = 0;
 
@@ -29,35 +50,24 @@ export const ArticleToc = ({ toc }: Props) => {
     };
 
     const onScroll = (): void => {
-
-      for (let i = 0; i < toc.length - 1; i++) {
-        if (isWindowBetween(document.getElementById(toc[i + 1].id!))) {
+      for (let i = 0; i < tocItemElements.length - 1; i++) {
+        if (isWindowBetween(document.getElementById(tocItemElements[i + 1].getAttribute("data-tocid")!))) {
           setActive(i);
           return;
         }
       }
-      setActive(toc.length - 1);
+      setActive(tocItemElements.length - 1);
     };
 
-    if (toc.length === 0) { return; }
 
-    toc.forEach((heading) => {
-      // add heading element
-      const tocEl = document.getElementById(`tocitem-${heading.id}`);
-      if (tocEl) {
-        tocItemElements.push(tocEl);
-
-      }
-    });
-
-    setActive(0);
+    tocItemElements[0].classList.add("active");
 
     window.addEventListener("scroll", onScroll);
 
     return () => {
       window.removeEventListener("scroll", onScroll);
     };
-  }, [toc]);
+  }, []);
 
 
   // TODO make overflow menu item break line
@@ -73,13 +83,7 @@ export const ArticleToc = ({ toc }: Props) => {
           </div>
           <ul className="menu">
             {
-              toc.map((x, i) => (
-                <li key={i} style={{ paddingLeft: (x.depth - 1) * 4 }}>
-                  <a id={`tocitem-${x.id}`} href={"#" + x.id}>
-                    {x.value}
-                  </a>
-                </li>
-              ))
+              toc.map((x) => <TocMenuEntry key={x.id} entry={x} />)
             }
           </ul>
         </li>
