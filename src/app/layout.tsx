215bee67e2c8a7e45983e66fa87f858332c3f06c
Chen Junda <ddadaal@outlook.com>
2023-12-13 20:06:51 +0800

    use analytix analysis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import "./globals.css";
 
 import { Metadata } from "next";
+import Script from "next/script";
 import { RootLayout } from "src/app/RootLayout";
 import { ToTop } from "src/components/ToTop";
 import { getResume } from "src/data/resume";
 import { Footer } from "src/layout/Footer";
-import { Header } from "src/layout/Header";
+import { Header } from "src/layout/Header"; ;
 
 export const metadata: Metadata = {
   title: "ddadaal.me",
@@ -19,6 +20,14 @@ export default async function Layout({ children }: { children: React.ReactNode }
 
   return (
     <RootLayout>
+      <Script
+        data-host="https://analytix.tk"
+        data-dnt="false"
+        src="https://analytix.linkspreed.com/js/script.js"
+        id="ZwSg9rf6GA"
+        async
+        defer
+      />
       <Header resumeLangs={resume?.langVersions.map((x) => x.lang) ?? []} />
       <div>
         {children}
